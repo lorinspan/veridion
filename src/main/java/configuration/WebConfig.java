d81b77cd9d6cfa90b2e028ef6d8cb67d560037d9
lorinspan <lorinspanx@gmail.com>
2024-02-28 23:17:41 +0200

    Added WebConfig for FE-BE communication

---
+++ src/main/java/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:4200") // Add your Angular app URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
